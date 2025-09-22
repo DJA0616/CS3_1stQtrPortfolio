7c5427a4272fc9bdf10a539089e67fb35cfca3ff
DJA0616 <b2029djfasis@pshs.edu.ph>
2025-09-22 10:22:15 +0800

    Removed test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-require('readline').createInterface({input: process.stdin,}).on('line', execute)
-
-function execute(input){
-    if(parseInt(input)){
-        console.log(parseInt(input) + 1 + `ay${12 + 3}`)
-    }
-}
